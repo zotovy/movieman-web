efb4d55834fc4a4f755da74cc45de9a3f52c7be6
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-03-28 18:46:40 +0500

    fix reducers

--- redux/store.ts
+++ redux/store.ts
@@ -4,9 +4,14 @@ import { MakeStore, createWrapper, Context } from "next-redux-wrapper";
 import rootReducer, { State } from "./reducers/root";
 
 import { InitialState as DiscoverState } from "@/redux/reducers/discover-reducer";
+import { InitialState as ReviewState } from "@/redux/reducers/review-reducer";
+import { InitialState as SearchState } from "@/redux/reducers/search-reducer";
+
 
 const initialState: State = {
     discoverReducer: DiscoverState,
+    reviewReducer: ReviewState,
+    searchReducer: SearchState,
 };
 
 const makeStore: MakeStore<State> = (context: Context) => {
