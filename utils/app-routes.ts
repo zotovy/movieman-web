411d7969842c22db42ebc1985a11f8e307a2ce02
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-03-31 21:04:01 +0500

    fix app routes

--- utils/app-routes.ts
+++ utils/app-routes.ts
@@ -2,8 +2,8 @@ export default class AppRoutes {
 
     static homepage = "/";
     static profile = "/profile";
-    static signup = "/profile";
-    static login = "/profile";
+    static signup = "/signup";
+    static login = "/login";
     static movieDetail = (id: number | string) => `/movie/${id}`;
     static writeReview = (id: number | string) => `/movie/${id}/review`;
     static reviewDetail = (mid: number | string, rid: number | string) => `/movie/${mid}/review/${rid}`;
